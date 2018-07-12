3fd15293ced3b46dc9cb354bfad4ae854ca9cec0
Matthew Woodruff <mwoodruf@thoughtworks.com>
2018-07-12 12:17:37 +0100

    Extract remaining items to functions

--- creek_and_continent.go
+++ creek_and_continent.go
@@ -19,9 +19,56 @@ func (g *CreekAndContinent) normalTick() {
 	}
 }
 
+func (g *CreekAndContinent) japaneseDenimTick() {
+	g.SellIn -= 1
+	g.Price += 1
+
+	if g.SellIn <= 0 {
+		g.Price += 1
+	}
+
+	if g.Price > 50 {
+		g.Price = 50
+	}
+}
+
+func (g *CreekAndContinent) whiteVestTopTick() {
+	return
+}
+
+func (g *CreekAndContinent) fashionShowTick() {
+	g.SellIn -= 1
+	if g.SellIn < 0 {
+		g.Price = 0
+		return
+	}
+	g.Price += 1
+	if g.SellIn < 10 {
+		g.Price += 1
+	}
+	if g.SellIn < 5 {
+		g.Price += 1
+	}
+	if g.Price > 50 {
+		g.Price = 50
+	}
+}
+
 func (g *CreekAndContinent) tick() {
 	if g.Name == "normal" {
 		g.normalTick()
+		return
+	}
+	if g.Name == "Japanese Denim" {
+		g.japaneseDenimTick()
+		return
+	}
+	if g.Name == "White Vest Top" {
+		g.whiteVestTopTick()
+		return
+	}
+	if g.Name == "Fashion Show Tickets" {
+		g.fashionShowTick()
 		return
 	}
 
