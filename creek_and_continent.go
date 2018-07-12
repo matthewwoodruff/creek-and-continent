ab6d4c1d9fbff424d5c6282d6df21c659f636405
Matthew Woodruff <mwoodruf@thoughtworks.com>
2018-07-12 11:47:46 +0100

    Update quality to price

--- creek_and_continent.go
+++ creek_and_continent.go
@@ -1,30 +1,30 @@
 package CreekAndContinent
 
 type CreekAndContinent struct {
-	Name    string
-	Quality int
-	SellIn  int
+	Name   string
+	Price  int
+	SellIn int
 }
 
 func (g *CreekAndContinent) tick() {
 	if g.Name != "Japanese Demin" && g.Name != "Fashion Show Tickets" {
-		if g.Quality > 0 {
+		if g.Price > 0 {
 			if g.Name != "White Vest Top" {
-				g.Quality = g.Quality - 1
+				g.Price = g.Price - 1
 			}
 		}
 	} else {
-		if g.Quality < 50 {
-			g.Quality = g.Quality + 1
+		if g.Price < 50 {
+			g.Price = g.Price + 1
 			if g.Name == "Fashion Show Tickets" {
 				if g.SellIn < 11 {
-					if g.Quality < 50 {
-						g.Quality = g.Quality + 1
+					if g.Price < 50 {
+						g.Price = g.Price + 1
 					}
 				}
 				if g.SellIn < 6 {
-					if g.Quality < 50 {
-						g.Quality = g.Quality + 1
+					if g.Price < 50 {
+						g.Price = g.Price + 1
 					}
 				}
 			}
@@ -38,17 +38,17 @@ func (g *CreekAndContinent) tick() {
 	if g.SellIn < 0 {
 		if g.Name != "Japanese Demin" {
 			if g.Name != "Fashion Show Tickets" {
-				if g.Quality > 0 {
+				if g.Price > 0 {
 					if g.Name != "White Vest Top" {
-						g.Quality = g.Quality - 1
+						g.Price = g.Price - 1
 					}
 				}
 			} else {
-				g.Quality = g.Quality - g.Quality
+				g.Price = g.Price - g.Price
 			}
 		} else {
-			if g.Quality < 50 {
-				g.Quality = g.Quality + 1
+			if g.Price < 50 {
+				g.Price = g.Price + 1
 			}
 		}
 	}
--- creek_and_continent_test.go
+++ creek_and_continent_test.go
@@ -6,320 +6,320 @@ import (
 
 func TestUpdatesNormalItemsBeforeSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "normal",
-		Quality: 10,
-		SellIn:  5,
+		Name:   "normal",
+		Price:  10,
+		SellIn: 5,
 	}
 	item.tick()
-	assertEquals(t, 9, item.Quality)
+	assertEquals(t, 9, item.Price)
 	assertEquals(t, 4, item.SellIn)
 }
 
 func TestUpdatesNormalItemsOnTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "normal",
-		Quality: 10,
-		SellIn:  0,
+		Name:   "normal",
+		Price:  10,
+		SellIn: 0,
 	}
 	item.tick()
-	assertEquals(t, 8, item.Quality)
+	assertEquals(t, 8, item.Price)
 	assertEquals(t, -1, item.SellIn)
 }
 
 func TestUpdatesNormalItemsAfterTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "normal",
-		Quality: 10,
-		SellIn:  -5,
+		Name:   "normal",
+		Price:  10,
+		SellIn: -5,
 	}
 	item.tick()
-	assertEquals(t, 8, item.Quality)
+	assertEquals(t, 8, item.Price)
 	assertEquals(t, -6, item.SellIn)
 }
 
-func TestUpdatesNormalItemsWithQualityOf0(t *testing.T) {
+func TestUpdatesNormalItemsWithPriceOf0(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "normal",
-		Quality: 0,
-		SellIn:  5,
+		Name:   "normal",
+		Price:  0,
+		SellIn: 5,
 	}
 	item.tick()
-	assertEquals(t, 0, item.Quality)
+	assertEquals(t, 0, item.Price)
 	assertEquals(t, 4, item.SellIn)
 }
 
 func TestUpdatesJapaneseDenimItemsBeforeTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Japanese Demin",
-		Quality: 10,
-		SellIn:  5,
+		Name:   "Japanese Demin",
+		Price:  10,
+		SellIn: 5,
 	}
 	item.tick()
-	assertEquals(t, 11, item.Quality)
+	assertEquals(t, 11, item.Price)
 	assertEquals(t, 4, item.SellIn)
 }
 
-func TestUpdatesJapaneseDenimItemsBeforeTheSellDateWithMaxQuality(t *testing.T) {
+func TestUpdatesJapaneseDenimItemsBeforeTheSellDateWithMaxPrice(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Japanese Demin",
-		Quality: 50,
-		SellIn:  5,
+		Name:   "Japanese Demin",
+		Price:  50,
+		SellIn: 5,
 	}
 	item.tick()
-	assertEquals(t, 50, item.Quality)
+	assertEquals(t, 50, item.Price)
 	assertEquals(t, 4, item.SellIn)
 }
 
 func TestUpdatesJapaneseDenimItemsOnTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Japanese Demin",
-		Quality: 10,
-		SellIn:  0,
+		Name:   "Japanese Demin",
+		Price:  10,
+		SellIn: 0,
 	}
 	item.tick()
-	assertEquals(t, 12, item.Quality)
+	assertEquals(t, 12, item.Price)
 	assertEquals(t, -1, item.SellIn)
 }
 
-func TestUpdatesJapaneseDenimItemsOnTheSellDateNearMaxQuality(t *testing.T) {
+func TestUpdatesJapaneseDenimItemsOnTheSellDateNearMaxPrice(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Japanese Demin",
-		Quality: 49,
-		SellIn:  0,
+		Name:   "Japanese Demin",
+		Price:  49,
+		SellIn: 0,
 	}
 	item.tick()
-	assertEquals(t, 50, item.Quality)
+	assertEquals(t, 50, item.Price)
 	assertEquals(t, -1, item.SellIn)
 }
 
-func TestUpdatesJapaneseDenimItemsOnTheSellDateWithMaxQuality(t *testing.T) {
+func TestUpdatesJapaneseDenimItemsOnTheSellDateWithMaxPrice(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Japanese Demin",
-		Quality: 50,
-		SellIn:  0,
+		Name:   "Japanese Demin",
+		Price:  50,
+		SellIn: 0,
 	}
 	item.tick()
-	assertEquals(t, 50, item.Quality)
+	assertEquals(t, 50, item.Price)
 	assertEquals(t, -1, item.SellIn)
 }
 
 func TestUpdatesJapaneseDenimItemsAfterTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Japanese Demin",
-		Quality: 10,
-		SellIn:  -10,
+		Name:   "Japanese Demin",
+		Price:  10,
+		SellIn: -10,
 	}
 	item.tick()
-	assertEquals(t, 12, item.Quality)
+	assertEquals(t, 12, item.Price)
 	assertEquals(t, -11, item.SellIn)
 }
 
-func TestUpdatesJapaneseDenimItemsAfterTheSellDateWithMaxQuality(t *testing.T) {
+func TestUpdatesJapaneseDenimItemsAfterTheSellDateWithMaxPrice(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Japanese Demin",
-		Quality: 50,
-		SellIn:  -10,
+		Name:   "Japanese Demin",
+		Price:  50,
+		SellIn: -10,
 	}
 	item.tick()
-	assertEquals(t, 50, item.Quality)
+	assertEquals(t, 50, item.Price)
 	assertEquals(t, -11, item.SellIn)
 }
 
 func TestUpdatesWhiteVestTopItemsBeforeTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "White Vest Top",
-		Quality: 10,
-		SellIn:  5,
+		Name:   "White Vest Top",
+		Price:  10,
+		SellIn: 5,
 	}
 	item.tick()
-	assertEquals(t, 10, item.Quality)
+	assertEquals(t, 10, item.Price)
 	assertEquals(t, 5, item.SellIn)
 }
 
 func TestUpdatesWhiteVestTopItemsOnTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "White Vest Top",
-		Quality: 10,
-		SellIn:  5,
+		Name:   "White Vest Top",
+		Price:  10,
+		SellIn: 5,
 	}
 	item.tick()
-	assertEquals(t, 10, item.Quality)
+	assertEquals(t, 10, item.Price)
 	assertEquals(t, 5, item.SellIn)
 }
 
 func TestUpdatesWhiteVestTopItemsAfterTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "White Vest Top",
-		Quality: 10,
-		SellIn:  -1,
+		Name:   "White Vest Top",
+		Price:  10,
+		SellIn: -1,
 	}
 	item.tick()
-	assertEquals(t, 10, item.Quality)
+	assertEquals(t, 10, item.Price)
 	assertEquals(t, -1, item.SellIn)
 }
 
 func TestUpdatesFashionShowTicketsItemsBeforeTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Fashion Show Tickets",
-		Quality: 10,
-		SellIn:  11,
+		Name:   "Fashion Show Tickets",
+		Price:  10,
+		SellIn: 11,
 	}
 	item.tick()
-	assertEquals(t, 11, item.Quality)
+	assertEquals(t, 11, item.Price)
 	assertEquals(t, 10, item.SellIn)
 }
 
 func TestUpdatesFashionShowTicketsItemsCloseToTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Fashion Show Tickets",
-		Quality: 10,
-		SellIn:  10,
+		Name:   "Fashion Show Tickets",
+		Price:  10,
+		SellIn: 10,
 	}
 	item.tick()
-	assertEquals(t, 12, item.Quality)
+	assertEquals(t, 12, item.Price)
 	assertEquals(t, 9, item.SellIn)
 }
 
-func TestUpdatesFashionShowTicketsItemsBeforeTheSellDateAtMaxQuality(t *testing.T) {
+func TestUpdatesFashionShowTicketsItemsBeforeTheSellDateAtMaxPrice(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Fashion Show Tickets",
-		Quality: 50,
-		SellIn:  10,
+		Name:   "Fashion Show Tickets",
+		Price:  50,
+		SellIn: 10,
 	}
 	item.tick()
-	assertEquals(t, 50, item.Quality)
+	assertEquals(t, 50, item.Price)
 	assertEquals(t, 9, item.SellIn)
 }
 
 func TestUpdatesFashionShowTicketsItemsVeryCloseToTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Fashion Show Tickets",
-		Quality: 10,
-		SellIn:  5,
+		Name:   "Fashion Show Tickets",
+		Price:  10,
+		SellIn: 5,
 	}
 	item.tick()
-	assertEquals(t, 13, item.Quality)
+	assertEquals(t, 13, item.Price)
 	assertEquals(t, 4, item.SellIn)
 }
 
-func TestUpdatesFashionShowTicketsItemsVeryCloseToTheSellDateAtMaxQuality(t *testing.T) {
+func TestUpdatesFashionShowTicketsItemsVeryCloseToTheSellDateAtMaxPrice(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Fashion Show Tickets",
-		Quality: 50,
-		SellIn:  5,
+		Name:   "Fashion Show Tickets",
+		Price:  50,
+		SellIn: 5,
 	}
 	item.tick()
-	assertEquals(t, 50, item.Quality)
+	assertEquals(t, 50, item.Price)
 	assertEquals(t, 4, item.SellIn)
 }
 
 func TestUpdatesFashionShowTicketsItemsWithOneDayLeftToSell(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Fashion Show Tickets",
-		Quality: 10,
-		SellIn:  1,
+		Name:   "Fashion Show Tickets",
+		Price:  10,
+		SellIn: 1,
 	}
 	item.tick()
-	assertEquals(t, 13, item.Quality)
+	assertEquals(t, 13, item.Price)
 	assertEquals(t, 0, item.SellIn)
 }
 
-func TestUpdatesFashionShowTicketsItemsWithOneDayLeftToSellAtMaxQuality(t *testing.T) {
+func TestUpdatesFashionShowTicketsItemsWithOneDayLeftToSellAtMaxPrice(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Fashion Show Tickets",
-		Quality: 50,
-		SellIn:  1,
+		Name:   "Fashion Show Tickets",
+		Price:  50,
+		SellIn: 1,
 	}
 	item.tick()
-	assertEquals(t, 50, item.Quality)
+	assertEquals(t, 50, item.Price)
 	assertEquals(t, 0, item.SellIn)
 }
 
 func TestUpdatesFashionShowTicketsItemsOnSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Fashion Show Tickets",
-		Quality: 10,
-		SellIn:  0,
+		Name:   "Fashion Show Tickets",
+		Price:  10,
+		SellIn: 0,
 	}
 	item.tick()
-	assertEquals(t, 0, item.Quality)
+	assertEquals(t, 0, item.Price)
 	assertEquals(t, -1, item.SellIn)
 }
 
 func TestUpdatesFashionShowTicketsItemsAfterTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Fashion Show Tickets",
-		Quality: 10,
-		SellIn:  -1,
+		Name:   "Fashion Show Tickets",
+		Price:  10,
+		SellIn: -1,
 	}
 	item.tick()
-	assertEquals(t, 0, item.Quality)
+	assertEquals(t, 0, item.Price)
 	assertEquals(t, -2, item.SellIn)
 }
 
 func _TestUpdatesConjuredItemsBeforeTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Homeware Hand Towel",
-		Quality: 10,
-		SellIn:  10,
+		Name:   "Homeware Hand Towel",
+		Price:  10,
+		SellIn: 10,
 	}
 	item.tick()
-	assertEquals(t, 8, item.Quality)
+	assertEquals(t, 8, item.Price)
 	assertEquals(t, 9, item.SellIn)
 }
 
-func _TestUpdatesHomewareHandTowelItemsAtZeroQuality(t *testing.T) {
+func _TestUpdatesHomewareHandTowelItemsAtZeroPrice(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Homeware Hand Towel",
-		Quality: 0,
-		SellIn:  10,
+		Name:   "Homeware Hand Towel",
+		Price:  0,
+		SellIn: 10,
 	}
 	item.tick()
-	assertEquals(t, 0, item.Quality)
+	assertEquals(t, 0, item.Price)
 	assertEquals(t, 9, item.SellIn)
 }
 
 func _TestUpdatesHomewareHandTowelItemsOnTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Homeware Hand Towel",
-		Quality: 10,
-		SellIn:  0,
+		Name:   "Homeware Hand Towel",
+		Price:  10,
+		SellIn: 0,
 	}
 	item.tick()
-	assertEquals(t, 6, item.Quality)
+	assertEquals(t, 6, item.Price)
 	assertEquals(t, -1, item.SellIn)
 }
 
-func _TestUpdatesHomewareHandTowelItemsBeforeTheSellDateAtZeroQuality(t *testing.T) {
+func _TestUpdatesHomewareHandTowelItemsBeforeTheSellDateAtZeroPrice(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Homeware Hand Towel",
-		Quality: 0,
-		SellIn:  0,
+		Name:   "Homeware Hand Towel",
+		Price:  0,
+		SellIn: 0,
 	}
 	item.tick()
-	assertEquals(t, 0, item.Quality)
+	assertEquals(t, 0, item.Price)
 	assertEquals(t, -1, item.SellIn)
 }
 
 func _TestUpdatesHomewareHandTowelItemsAfterTheSellDate(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Homeware Hand Towel",
-		Quality: 10,
-		SellIn:  -10,
+		Name:   "Homeware Hand Towel",
+		Price:  10,
+		SellIn: -10,
 	}
 	item.tick()
-	assertEquals(t, 6, item.Quality)
+	assertEquals(t, 6, item.Price)
 	assertEquals(t, -11, item.SellIn)
 }
 
-func _TestUpdatesHomewareHandTowelItemsAfterTheSellDateAtZeroQuality(t *testing.T) {
+func _TestUpdatesHomewareHandTowelItemsAfterTheSellDateAtZeroPrice(t *testing.T) {
 	item := CreekAndContinent{
-		Name:    "Homeware Hand Towel",
-		Quality: 0,
-		SellIn:  10,
+		Name:   "Homeware Hand Towel",
+		Price:  0,
+		SellIn: 10,
 	}
 	item.tick()
-	assertEquals(t, 0, item.Quality)
+	assertEquals(t, 0, item.Price)
 	assertEquals(t, -11, item.SellIn)
 }
 
